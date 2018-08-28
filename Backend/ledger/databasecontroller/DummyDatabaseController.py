63e7481be0c32964c04a23441e59421b2445f12d
Mark Zeagler <mark.zeagler@outlook.com>
2018-08-28 19:39:44 -0400

    Expanded dummy database controller

--- Backend/ledger/databasecontroller/DummyDatabaseController.py
+++ Backend/ledger/databasecontroller/DummyDatabaseController.py
@@ -6,7 +6,19 @@ class DummyDatabaseController(AbstractDatabaseController):
         AbstractDatabaseController.__init__(self)
 
     def get_user_auth_token(self, username, password_hash):
+        if username is "admin":
+            return 1000
+        if username is "manager":
+            return 500
+        if username is "user":
+            return 250
         return 100
 
     def get_user_id(self, username, password_hash):
+        if username is "admin":
+            return 100
+        if username is "manager":
+            return 50
+        if username is "user":
+            return 25
         return 1
