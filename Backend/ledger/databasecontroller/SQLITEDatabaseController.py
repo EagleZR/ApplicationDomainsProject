5181f7321b0cb78e907b444236d3daf83db64857
Mark Zeagler <mark.zeagler@outlook.com>
2018-08-30 14:11:53 -0400

    Fixed database insert statement

--- Backend/ledger/databasecontroller/SQLITEDatabaseController.py
+++ Backend/ledger/databasecontroller/SQLITEDatabaseController.py
@@ -13,44 +13,49 @@ class SQLITEDatabaseController(AbstractDatabaseController):
         self.log.setLevel(logging.getLevelName('INFO'))
         if not os.path.isfile(database_file_name):
             self.log.warning("The database did not exist, a new one is being created.")
-        self.db = sqlite3.connect(database_file_name)
         self.verify_tables_columns()
 
     def verify_tables_columns(self):
         # Make sure each table exists and possess the correct columns. If possible, add the tables.
         self.log.info("Verifying database structure.")
-        cursor = self.db.cursor()
+        db = sqlite3.connect(database_file_name)
+        cursor = db.cursor()
         try:
             cursor.execute('''SELECT * FROM Users''')
-            cursor.close()
             self.log.warning("User table already exists.")
         except sqlite3.OperationalError:
             self.log.warning("Creating user table.")
             cursor.execute(
-                '''Create Table Users(USER_ID integer primary key autoincrement, USERNAME varchar(40) not null, 
-                PASSWORD_HASH varchar(100) not null, AUTH_TOKEN varchar(100) not null, ACCOUNT_TYPE varchar(10) 
-                not null )''')
-            print('''Insert into Users(USERNAME, PASSWORD_HASH, AUTH_TOKEN, ACCOUNT_TYPE) values (%s, %s, %s, %s)''' %
-                  ("u32ser", "asdkjhasdkjh", get_auth_token(), "user"))
-            cursor.execute(
-                '''Insert into Users(USERNAME, PASSWORD_HASH, AUTH_TOKEN, ACCOUNT_TYPE) values (%s, %s, %s, %s)''' %
-                ("u32ser", "asdkjhasdkjh", get_auth_token(), "user"))
-            cursor.execute('''SELECT * FROM Users''')
-            self.db.commit()
+                '''Create Table If Not Exists Users(USER_ID integer primary key autoincrement, USERNAME TEXT not null, 
+                PASSWORD_HASH TEXT not null, AUTH_TOKEN TEXT not null, ACCOUNT_TYPE Text not null );''')
+            # print('''Insert into Users(USERNAME, PASSWORD_HASH, AUTH_TOKEN, ACCOUNT_TYPE) values (%s, %s, %s, %s);''' %
+            #       ("u32ser", "asdkjhasdkjh", get_auth_token(), "user"))
+            # cursor.execute(
+            #     '''Insert into Users(USERNAME, PASSWORD_HASH, AUTH_TOKEN, ACCOUNT_TYPE) values (%s, %s, %s, %s);''' %
+            #     ("u32ser", "asdkjhasdkjh", get_auth_token(), "user"))
+            # cursor.execute('''SELECT * FROM Users;''')
+            db.commit()
+        cursor.close()
+        db.close()
         # TODO Add tables as they're designed
 
     def add_user(self, username, password_hash, account_type="deactivated"):
         if account_type not in self.account_types:
             raise InvalidUserType("The user type " + account_type + " is not an approved account type")
-        cursor = self.db.cursor()
-        cursor.execute(
-            '''Insert into Users (USERNAME, PASSWORD_HASH, AUTH_TOKEN, ACCOUNT_TYPE) values (%s, %s, %s, %s)''' %
-            (username, password_hash, get_auth_token(), account_type))
+        db = sqlite3.connect(database_file_name)
+        cursor = db.cursor()
+        dictionary = {"username": username, "password_hash": password_hash, "auth_token": get_auth_token(),
+                      "account_type": account_type}
+        insert_text = '''Insert into Users (USERNAME, PASSWORD_HASH, AUTH_TOKEN, ACCOUNT_TYPE) values
+            ('{username}', '{password_hash}', '{auth_token}', '{account_type}');'''.format(**dictionary)
+        cursor.execute(insert_text)
+        db.commit()
         cursor.close()
-        self.db.commit()
+        db.close()
 
     def get_user_id(self, username, password_hash):
-        cursor = self.db.cursor()
+        db = sqlite3.connect(database_file_name)
+        cursor = db.cursor()
         cursor.execute(
             '''Select USER_ID from USERS where USERNAME = '%s' and PASSWORD_HASH = '%s' ''' % (username, password_hash))
         results = list()
@@ -64,7 +69,8 @@ class SQLITEDatabaseController(AbstractDatabaseController):
         return results[0]
 
     def get_user_auth_token(self, username, password_hash):
-        cursor = self.db.cursor()
+        db = sqlite3.connect(database_file_name)
+        cursor = db.cursor()
         cursor.execute(
             '''Select AUTH_TOKEN from USERS where USERNAME = '?' and PASSWORD_HASH = '?' ''', (username, password_hash))
         results = list()
@@ -74,9 +80,6 @@ class SQLITEDatabaseController(AbstractDatabaseController):
         if len(results) is 0:
             return None
         return results[0]
-
-    def close(self):
-        self.db.close()
 
 
 class InvalidUserType(Exception):
