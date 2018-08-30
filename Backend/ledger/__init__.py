ae006e177f56b8e2fdb55e82710e709b39abd84f
Mark Zeagler <mark.zeagler@outlook.com>
2018-08-30 17:56:15 -0400

    Added backend register address handler

--- Backend/ledger/__init__.py
+++ Backend/ledger/__init__.py
@@ -32,17 +32,33 @@ def login():
     if request.method == 'POST':
         json_data = request.get_json()
         if json_data is not None:
-            username = json_data.get('username')
-            password_hash = json_data.get('password_hash')
-            user_id = db.get_user_id(username, password_hash)
-            auth_token = db.get_user_auth_token(username, password_hash)
+            email = json_data.get('email')
+            password = json_data.get('password')
+            user_id = db.get_user_id(email, password)
+            auth_token = db.get_user_auth_token(email, password)
             if (user_id is None) or (auth_token is None):
-                raise get_error_response(403, "The username/password is invalid")
+                raise get_error_response(403, "The email/password is invalid")
             response = jsonify({"user_id": user_id, "auth_token": auth_token})
             response.status_code = 200
             return response
         else:
             raise get_error_response(400, "Please include both the username and the password")
+    else:
+        raise get_error_response(400, "Only POST requests are valid for this address")
+
+
+@app.route('/register', methods=['POST'])
+def register():
+    log.info("Got a " + request.method + " for " + request.url + "from " + request.host_url)
+    if request.method == 'POST':
+        json_data = request.get_json()
+        if json_data is not None:
+            email = json_data.get('email')
+            password = json_data.get('password')
+            name = json_data.get('name')
+            db.add_user(email, password, name)
+        else:
+            raise get_error_response(400, "Please include the email, password, ")
     else:
         raise get_error_response(400, "Only POST requests are valid for this address")
 
