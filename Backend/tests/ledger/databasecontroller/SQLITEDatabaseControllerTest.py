dd25bba84c36ccf268b7ae66d73609976511036e
Mark Zeagler <mark.zeagler@outlook.com>
2018-08-30 18:31:07 -0400

    Fixed SQLITE controller test

--- Backend/tests/ledger/databasecontroller/SQLITEDatabaseControllerTest.py
+++ Backend/tests/ledger/databasecontroller/SQLITEDatabaseControllerTest.py
@@ -10,7 +10,7 @@ class SQLITEDatabaseControllerTest(unittest.TestCase):
 
     """
 
-    def setUpClass(self):
+    def setUp(self):
         database_file_path = os.getcwd() + '\\..\\..\\..\\ledger\\databasecontroller\\sqlitedb.db'
         if os.path.isfile(database_file_path):
             os.remove(database_file_path)
@@ -20,23 +20,26 @@ class SQLITEDatabaseControllerTest(unittest.TestCase):
     def test_get_user_id(self):
         db = SQLITEDatabaseController()
         email = "user1"
-        password_hash = "dfiuohsdfiu"
-        db.add_user(email, password_hash)
-        db.get_user_id(email, password_hash)
+        password = "dfiuohsdfiu"
+        name = "Roger"
+        db.add_user(email, password, name)
+        db.get_user_id(email, password)
         # Only fails if an exception was thrown
 
     def test_get_auth_token(self):
         db = SQLITEDatabaseController()
         email = "user2"
-        password_hash = "efbkwbkwe"
-        db.add_user(email, password_hash)
-        db.get_user_auth_token(email, password_hash)
+        password = "efbkwbkwe"
+        name = "Emily"
+        db.add_user(email, password, name)
+        db.get_user_auth_token(email, password)
         # Only fails if an exception was thrown
 
     def test_duplicate_email_exception(self):
         db = SQLITEDatabaseController()
         email = "user3"
-        password_hash = "fdkijhsdsf"
-        db.add_user(email, password_hash)
+        password = "fdkijhsdsf"
+        name = "Antonio"
+        db.add_user(email, password, name)
         with self.assertRaises(DuplicateEmailException) as context:
-            db.add_user(email, password_hash)
+            db.add_user(email, password, name)
