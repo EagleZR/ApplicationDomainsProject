400ca79be18c3802df6125876cda95968fb8f910
Mark Zeagler <mark.zeagler@outlook.com>
2018-10-05 00:26:18 -0400

    Added default error messages

--- postit/HTTPError.py
+++ postit/HTTPError.py
@@ -1,5 +1,5 @@
 class HTTPError(Exception):
-    def __init__(self, message, status_code=400):
+    def __init__(self, message="default message", status_code=400):
         Exception.__init__(self)
         self.message = message
         self.status_code = status_code
