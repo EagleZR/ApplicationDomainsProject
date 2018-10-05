a2e96a4a1d3c2a30a8c202439eebf00bb9c4ea83
Mark Zeagler <mark.zeagler@outlook.com>
2018-10-05 02:15:11 -0400

    Added random last_login seeds

--- db_setup.py
+++ db_setup.py
@@ -1,12 +1,20 @@
 import configparser
 import os.path
 import logging
+import random
+from datetime import (datetime, timedelta)
 import re
 
 from postit import databasecontroller
 
 logging.basicConfig(filename="/var/www/markzeagler.com/postit.log", datefmt="%d-%b-%Y %H:%M:%S", level=logging.DEBUG,
                     format='%(asctime)s %(levelname)s %(message)s')
+
+
+# https://stackoverflow.com/questions/553303/generate-a-random-date-between-two-other-dates
+def random_date(start, end, prop):
+    return start + prop * (end - start)
+
 
 config = configparser.ConfigParser()
 config.read(os.path.dirname(os.path.realpath(__file__)) + '/postit/config.ini')
@@ -19,4 +27,7 @@ with open(os.path.dirname(os.path.realpath(__file__)) + '/user_setup_data.txt', 
         username, password, email, first_name, last_name = line.split(', ')
         user_type = email_regex.search(email).group(1)
         db.add_user(username, password, email, first_name, last_name, db.get_30_days_from_now())
-        db.set_user_type(db.get_user_id(username), user_type)
+        user_id = db.get_user_id(username)
+        db.set_user_type(user_id, user_type)
+        db.update_last_login(user_id, random_date((datetime.today() - timedelta(days=30)), datetime.today(),
+                                                  random.random()))
