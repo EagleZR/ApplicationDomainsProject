a01b4f7bac5fdbc2d81705e7d816376b049323ba
Mark Zeagler <mark.zeagler@outlook.com>
2018-10-11 03:54:14 -0400

    Removed event log open/close

--- postit/EventLog.py
+++ postit/EventLog.py
@@ -13,7 +13,6 @@ def format_log_entry(log_entry):
 
 class EventLog:
     def __init__(self):
-        self.is_open = True  # TODO Why do I have this?
 
         if os.path.isfile(file_name):
             logging.info("EventLog already exists")
@@ -28,56 +27,39 @@ class EventLog:
             self.write(1, "Created Log")  # Attribute any automatic actions to the root admin
 
     def write(self, user_id, text):
-        if self.is_open:
-            logging.info("Writing to EventLog: \t" + text)
-            self.log.append({"user_id": user_id, "message": text,
-                             "timestamp": datetime.today().strftime(date_string_format)})
-            self.save()
+        logging.info("Writing to EventLog: \t" + text)
+        self.log.append({"user_id": user_id, "message": text,
+                         "timestamp": datetime.today().strftime(date_string_format)})
+        self.save()
 
     def read_last(self):
-        if self.is_open:
-            return format_log_entry(self.log[len(self.log) - 1])
-        return None
+        return format_log_entry(self.log[len(self.log) - 1])
 
     def read_all_as_text(self):
-        if self.is_open:
-            return_string = ""
-            for s in self.log:
-                return_string += format_log_entry(s)
-            return return_string
-        return None
+        return_string = ""
+        for s in self.log:
+            return_string += format_log_entry(s)
+        return return_string
 
     def get_all(self):
-        if self.is_open:
-            return self.log
-        return None
+        return self.log
 
     def read_from_user_as_text(self, user_id):
-        if self.is_open:
-            return_string = ""
-            for s in self.log:
-                if str(s['user_id']) == str(user_id):
-                    return_string += format_log_entry(s)
-            return return_string
-        return None
+        return_string = ""
+        for s in self.log:
+            if str(s['user_id']) == str(user_id):
+                return_string += format_log_entry(s)
+        return return_string
 
     def get_from_user(self, user_id):
-        if self.is_open:
-            return_list = list()
-            for s in self.log:
-                if str(s['user_id'] == user_id):
-                    return_list += s
-            return return_list
-        return None
-
-    def close(self):
-        if self.is_open:
-            self.save()
-            self.is_open = False
+        return_list = list()
+        for s in self.log:
+            if str(s['user_id'] == user_id):
+                return_list += s
+        return return_list
 
     def save(self):
-        if self.is_open:
-            data = pickle.dumps(self.log)
-            file = open(file_name, "wb+")
-            file.write(data)
-            file.close()
+        data = pickle.dumps(self.log)
+        file = open(file_name, "wb+")
+        file.write(data)
+        file.close()
