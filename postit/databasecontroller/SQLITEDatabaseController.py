0a6a56423615e1dd1126bdc6570e7abfc0ae371f
Mark Zeagler <mark.zeagler@outlook.com>
2018-10-27 22:26:07 -0400

    Revert "Moved transaction sorting to pre-insertion"
    This reverts commit 38d384f2301c0c05f29550038f06344745fbb3d8.

--- postit/databasecontroller/SQLITEDatabaseController.py
+++ postit/databasecontroller/SQLITEDatabaseController.py
@@ -631,6 +631,8 @@ class SQLITEDatabaseController(AbstractDatabaseController):
                 transactions_dicts.append({"account_id": transaction[0], "amount": transaction[1],
                                            "account_title": transaction[2]})
 
+            transactions_dicts.sort(reverse=True, key=get_transaction_amount)
+
             results_dict_list.append(
                 {"journal_entry_id": result[0], "user_id": result[1], "date": result[2],
                  "description": result[3], "type": result[4], "status": result[5], "posting_reference": result[6],
@@ -682,6 +684,8 @@ class SQLITEDatabaseController(AbstractDatabaseController):
                 transactions_dicts.append({"account_id": transaction[0], "amount": transaction[1],
                                            "account_title": transaction[2]})
 
+            transactions_dicts.sort(reverse=True, key=get_transaction_amount)
+
             results_dict_list.append(
                 {"journal_entry_id": result[0], "user_id": result[1], "date": result[2],
                  "description": result[3], "type": result[4], "status": result[5], "posting_reference": result[6],
@@ -712,3 +716,6 @@ def hash_password(password):
     m = hashlib.sha256()
     m.update(password.encode())
     return str(m.hexdigest())
+
+def get_transaction_amount(transaction):
+    return transaction['amount']
