aadc874ae75dcec7455ec3a2603dc6565f730280
Mark Zeagler <mark.zeagler@outlook.com>
2018-10-27 22:21:33 -0400

    Reversed transaction sorting

--- postit/databasecontroller/SQLITEDatabaseController.py
+++ postit/databasecontroller/SQLITEDatabaseController.py
@@ -631,6 +631,8 @@ class SQLITEDatabaseController(AbstractDatabaseController):
                 transactions_dicts.append({"account_id": transaction[0], "amount": transaction[1],
                                            "account_title": transaction[2]})
 
+            transactions_dicts.sort(reverse=True, key=get_transaction_amount)
+
             results_dict_list.append(
                 {"journal_entry_id": result[0], "user_id": result[1], "date": result[2],
                  "description": result[3], "type": result[4], "status": result[5], "posting_reference": result[6],
@@ -682,7 +684,7 @@ class SQLITEDatabaseController(AbstractDatabaseController):
                 transactions_dicts.append({"account_id": transaction[0], "amount": transaction[1],
                                            "account_title": transaction[2]})
 
-            transactions_dicts.sort(key=get_transaction_amount)
+            transactions_dicts.sort(reverse=True, key=get_transaction_amount)
 
             results_dict_list.append(
                 {"journal_entry_id": result[0], "user_id": result[1], "date": result[2],
