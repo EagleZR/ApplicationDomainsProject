e90fc9f7f1afacf4856887a080ef29d26d07da57
Mark Zeagler <mark.zeagler@outlook.com>
2018-10-27 21:40:34 -0400

    Fixed database data mislabeling

--- postit/databasecontroller/SQLITEDatabaseController.py
+++ postit/databasecontroller/SQLITEDatabaseController.py
@@ -628,8 +628,8 @@ class SQLITEDatabaseController(AbstractDatabaseController):
             transactions_dicts = list()
 
             for transaction in transactions:
-                transactions_dicts.append({"account_id": transaction[0], "account_title": transaction[1],
-                                           "amount": transaction[2]})
+                transactions_dicts.append({"account_id": transaction[0], "account_title": transaction[2],
+                                           "amount": transaction[1]})
 
             results_dict_list.append(
                 {"journal_entry_id": result[0], "user_id": result[1], "date": result[2],
